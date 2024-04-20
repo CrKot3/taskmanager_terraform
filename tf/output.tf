c807954ceed902b405e7c446500161157feb8a85
Кирилл <103645968+CrKot3@users.noreply.github.com>
2024-04-20 17:45:11 +0300

    Update output.tf

--- tf/output.tf
+++ tf/output.tf
@@ -1,5 +1,5 @@
 output "servers" {
   value = {
-    tg_server = openstack_compute_instance_v2.restaraunt_finder_tg.access_ip_v4
+    tg_server = openstack_compute_instance_v2.task_manager_tg.access_ip_v4
   }
 }
